1a80659404130fedd6d5618dd44084103aa12cc4
Grant Royseth <groyseth@gmail.com>
2022-01-03 22:10:36 -0600

    done with the main work, working on read me.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Weather-Dash-GR
+
+I started off with making the HTML file and started to style it with bootstrap.
+ I used a picture of dots as placeholders for the five day forcast, also placed placeholders for the data that i'm about to plug in.
+ 
+ 
--- assets/weather.js
+++ assets/weather.js
@@ -7,6 +7,10 @@ var humidity = document.querySelector('.humidity');
 var timeZone = document.querySelector('.timezone');
 var olSearch = document.getElementById('listCity');
 var citySearching = document.getElementById('citySearch');
+var cityHistory = [];
+if (localStorage.getItem("city")) {
+    cityHistory = JSON.parse(localStorage.getItem("city"))
+}
 
 // var long = "";
 // var latt = "";
@@ -17,37 +21,52 @@ function searchCityByInput(city) {
     // var long;
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey)
         .then(function (response) {
-            return response.json();
+           
+            
+             return response.json();
         })
         .then(function (data) {
+           if(!data.name){
+               
+               alert("not a city!")
+               return
+           }
+           
+               
+           
             console.log(data);
-            var tempValue = data['main']['temp'];
-            var nameInput = data['name'];
-            var windValue = data['wind']['speed'];
-            var humidityValue = data['main']['humidity']
+            // var tempValue = data['main']['temp'];
+            // var nameInput = data['name'];
+            // var windValue = data['wind']['speed'];
+            // var humidityValue = data['main']['humidity']
             // var timeValue = data['timezone']
-           var latt = data.coord.lat;
-           var long = data.coord.lon;
+            
 
 
-            nameVal.innerHTML = nameInput;
-            temp.innerHTML = "Temp: " + tempValue + " °F";
-            wind.innerHTML = "Wind: " + windValue + " MPH";
-            humidity.innerHTML = "Humidity: " + humidityValue + " %";
+            // nameVal.innerHTML = nameInput;
+            // temp.innerHTML = "Temp: " + tempValue + " °F";
+            // wind.innerHTML = "Wind: " + windValue + " MPH";
+            // humidity.innerHTML = "Humidity: " + humidityValue + " %";
+            $(".name").text(data.name);
+            $(".timezone").text(moment().format(" MM/D/YYYY"));
+            $(".temp").text("Temp: " + data.main.temp + "℉");
+            $(".wind").text("Wind: " + data.wind.speed + " MPH");
+            $(".humidity").text("Humidity: " + data.main.humidity + " %");
 
             // timeZone.innerHTML = timeValue;
             // timeZone.innerHTML = (new Date(data.dt*1000-(data.timezone*1000))); // minus
-            timeZone.innerHTML = moment().format(" MM/D/YYYY");
-            
+            // timeZone.innerHTML = moment().format(" MM/D/YYYY");
+            var latt = data.coord.lat;
+            var long = data.coord.lon;
 
-        
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ latt + "&lon="+ long + "&units=imperial&appid=" + APIKey)
-        .then(function (response) {
-            return response.json();
 
-        })
-        .then(function (data) {
-            $("#uvIndex").text("UV Index: " + data.current.uvi);
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + latt + "&lon=" + long + "&units=imperial&appid=" + APIKey)
+                .then(function (response) {
+                    return response.json();
+
+                })
+                .then(function (data) {
+                    $("#uvIndex").text("UV Index: " + data.current.uvi);
                     if (data.current.uvi < 3) {
                         $("#uvIndex").attr("class", "favorable");
                     } else if (data.current.uvi >= 3 && data.current.uvi <= 7) {
@@ -55,92 +74,46 @@ function searchCityByInput(city) {
                     } else {
                         $("#uvIndex").attr("class", "severe");
                     }
-            console.log(data);
-            $("#day1Date").text(moment().add(1, "days").format(" MM/D/YYYY"));
-            $("#day1Temp").text( "Temp: " + data.daily[1].temp.day + "℉");
-            $("#day1Wind").text("Wind: " + data.daily[1].wind_speed + " MPH");
-            $("#day1Humid").text("Humidity: " + data.daily[1].humidity + " %");
-            $("#img1").attr("src", "https://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png");
+                    console.log(data);
+                    $("#day1Date").text(moment().add(1, "days").format(" MM/D/YYYY"));
+                    $("#day1Temp").text("Temp: " + data.daily[1].temp.day + "℉");
+                    $("#day1Wind").text("Wind: " + data.daily[1].wind_speed + " MPH");
+                    $("#day1Humid").text("Humidity: " + data.daily[1].humidity + " %");
+                    $("#img1").attr("src", "https://openweathermap.org/img/w/" + data.daily[1].weather[0].icon + ".png");
 
-            $("#day2Date").text(moment().add(2, "days").format(" MM/D/YYYY"));
-            $("#day2Temp").text( "Temp: " + data.daily[2].temp.day + "℉");
-            $("#day2Wind").text("Wind: " + data.daily[2].wind_speed + " MPH");
-            $("#day2Humid").text("Humidity: " + data.daily[2].humidity + " %");
-            $("#img2").attr("src", "https://openweathermap.org/img/w/" + data.daily[2].weather[0].icon + ".png");
+                    $("#day2Date").text(moment().add(2, "days").format(" MM/D/YYYY"));
+                    $("#day2Temp").text("Temp: " + data.daily[2].temp.day + "℉");
+                    $("#day2Wind").text("Wind: " + data.daily[2].wind_speed + " MPH");
+                    $("#day2Humid").text("Humidity: " + data.daily[2].humidity + " %");
+                    $("#img2").attr("src", "https://openweathermap.org/img/w/" + data.daily[2].weather[0].icon + ".png");
 
-            $("#day3Date").text(moment().add(3, "days").format(" MM/D/YYYY"));
-            $("#day3Temp").text( "Temp: " + data.daily[3].temp.day + "℉");
-            $("#day3Wind").text("Wind: " + data.daily[3].wind_speed + " MPH");
-            $("#day3Humid").text("Humidity: " + data.daily[3].humidity + " %");
-            $("#img3").attr("src", "https://openweathermap.org/img/w/" + data.daily[3].weather[0].icon + ".png");
+                    $("#day3Date").text(moment().add(3, "days").format(" MM/D/YYYY"));
+                    $("#day3Temp").text("Temp: " + data.daily[3].temp.day + "℉");
+                    $("#day3Wind").text("Wind: " + data.daily[3].wind_speed + " MPH");
+                    $("#day3Humid").text("Humidity: " + data.daily[3].humidity + " %");
+                    $("#img3").attr("src", "https://openweathermap.org/img/w/" + data.daily[3].weather[0].icon + ".png");
 
-            $("#day4Date").text(moment().add(4, "days").format(" MM/D/YYYY"));
-            $("#day4Temp").text( "Temp: " + data.daily[4].temp.day + "℉");
-            $("#day4Wind").text("Wind: " + data.daily[4].wind_speed + " MPH");
-            $("#day4Humid").text("Humidity: " + data.daily[4].humidity + " %");
-            $("#img4").attr("src", "https://openweathermap.org/img/w/" + data.daily[4].weather[0].icon + ".png");
+                    $("#day4Date").text(moment().add(4, "days").format(" MM/D/YYYY"));
+                    $("#day4Temp").text("Temp: " + data.daily[4].temp.day + "℉");
+                    $("#day4Wind").text("Wind: " + data.daily[4].wind_speed + " MPH");
+                    $("#day4Humid").text("Humidity: " + data.daily[4].humidity + " %");
+                    $("#img4").attr("src", "https://openweathermap.org/img/w/" + data.daily[4].weather[0].icon + ".png");
 
-            $("#day5Date").text(moment().add(5, "days").format(" MM/D/YYYY"));
-            $("#day5Temp").text( "Temp: " + data.daily[5].temp.day + "℉");
-            $("#day5Wind").text("Wind: " + data.daily[5].wind_speed + " MPH");
-            $("#day5Humid").text("Humidity: " + data.daily[5].humidity + " %");
-            $("#img5").attr("src", "https://openweathermap.org/img/w/" + data.daily[5].weather[0].icon + ".png");
-        });
-    })
+                    $("#day5Date").text(moment().add(5, "days").format(" MM/D/YYYY"));
+                    $("#day5Temp").text("Temp: " + data.daily[5].temp.day + "℉");
+                    $("#day5Wind").text("Wind: " + data.daily[5].wind_speed + " MPH");
+                    $("#day5Humid").text("Humidity: " + data.daily[5].humidity + " %");
+                    $("#img5").attr("src", "https://openweathermap.org/img/w/" + data.daily[5].weather[0].icon + ".png");
+                });
+        })
+    
     // add catch!
+    // .catch(error => {
+    //     console.log(error)
+    // });
 
 }
-// function futureWeatherInput(city) {
-//     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey)
-//         .then(function (response) {
-//             return response.json();
 
-//         })
-//         .then(function (data) {
-
-//             console.log(data);
-
-//             // console.log("for")
-//             //  document.getElementById("day" + (i+1) + "Wind").innerHTML = "Wind: " + Number(data.list[i].wind.speed) + " MPH";
-//             //  document.getElementById("day" + (i+1) + "Temp").innerHTML = "Temp: " + Number(data.list[i].main.temp) + " °F";
-
-//             // }
-
-//             //    var latt = data.city.coord.lat;
-//             //    var long = data.city.coord.lon;
-//             //    console.log(latt)
-//             //     console.log(long)
-
-//             for (var i = 0; i < data.list; i += 8) {
-
-
-
-                
-
-//                 // var tempFuture = data['list']['main']['temp']
-//                 // tempOne.innerHTML = tempFuture[i];
-//                 document.getElementById("day" + (i + 1) + "Temp").innerHTML = "Temp: " + Number(data.list[i].main.temp) + " °F";
-
-//                 // document.getElementById("day" + (i + 1) + "Temp").innerHTML = "Temp: " +  + " °F";
-
-//                 document.getElementById("day" + (i + 1) + "Wind").innerHTML = "Wind: " + Number(data.list[i].wind.speed) + " MPH";
-
-//                 document.getElementById("day" + (i + 1) + "Humid").innerHTML = "Humidity: " + Number(data.list[i].wind.speed) + " %";
-
-//                 document.getElementById("img" + (i+1)).src = " http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
-
-//                 document.getElementById("day" + (i+1) + "Date").innerHTML = "Date: " + data.list[i].dt_txt;
-
-//             }
-
-//         })
-// }
-    //     TempDays.innerHTML = "Temp: " + Number(data.list[i].main.temp) + " °F";
-    //     console.log(TempDays)
-    // }
-
-
-//    for (i = 0; i < data.list.length; i += 8 ){
 
 
 
@@ -149,37 +122,63 @@ function getInputValue(e) {
     e.preventDefault()
     // Selecting the input element and get its value 
     var inputVal = document.getElementById("citySearch").value;
+   
     searchCityByInput(inputVal);
-    // city = inputVal.push[""];
     
-    var paragraph = document.createElement("p");
+    cityHistory.push(inputVal);
+     var cities = new Set(cityHistory);
+     cityHistory = Array.from(cities)
+    localStorage.setItem("city", JSON.stringify(cityHistory));
 
-    paragraph.innerHTML = inputVal;
-
-    paragraph.addEventListener("click" , function(e){
-        console.log(e.target.textContent)
-        searchCityByInput(e.target.textContent)
-    }) 
-
-    olSearch.appendChild(paragraph);
+    renderHistory()
     
-    console.log(city)
 
-
-
-    console.log("paragraph")
 
 }
 
+renderHistory()
+function renderHistory() {
+olSearch.innerHTML = ""
+    for (let i = 0; i < cityHistory.length; i++) {
+        // if(cityHistory[i] === cityHistory[i]){
+        //     console.log("yes")
+        // }else{
+        //     console.log("no")
+        // }
 
 
+        var paragraph = document.createElement("p");
 
+        paragraph.innerHTML = cityHistory[i];
+        
+        paragraph.addEventListener("click", function (e) {
+            console.log(e.target.textContent)
+            searchCityByInput(e.target.textContent)
+            
+            
+        })
+    //    if city equils it self then stop
+
+        olSearch.appendChild(paragraph);
+    }
+    
+}
+
+
+function deleteLocal(){
+    localStorage.clear()
+}
 document.querySelector(".subBtn").addEventListener("click", getInputValue)
 
 
 // document.querySelector(".subBtn").addEventListener("click", listInputValue)
 // timeOfCity()
 
+
+// logic cant dup city
+//   if statement
+// catch if not a city
+// clear btn
 
 // for (let i = 0; i < cars.length; i++)
 // make a loop
